b7f4862ffe38e3a437938964557e65d5593ffc08
algolov <algolov_dev@outlook.com>
2015-01-20 17:29:44 +1000

    setup scene with objects

---
+++ client/src/main/scala/client/Tutorial.scala
@@ -0,0 +1,77 @@
+package client
+
+import scalajs.js
+import js.annotation.JSExport
+import org.scalajs.threejs._
+
+import org.scalajs.dom
+import dom. { document, window }
+//import scalatags.JsDom.all._
+
+
+@JSExport
+object Tutorial {
+  @JSExport
+  def main() {
+    
+    val scene = new Scene
+    
+    val camera = new PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000)
+    
+    val renderer = new WebGLRenderer
+    
+    renderer.setClearColor(new Color().setHex(0xEEEEEE), 0)
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    
+    val axes = new AxisHelper(20)
+    
+    scene.add(axes)
+    
+    val planeGeometry = new PlaneGeometry(60, 20, 1, 1)
+    val planeMaterial = new MeshBasicMaterial(js.Dynamic.literal(
+      color = new Color().setHex(0xcccccc)
+    ).asInstanceOf[MeshBasicMaterialParameters])
+    
+    val plane = new Mesh(planeGeometry, planeMaterial)
+    
+    plane.rotation.x = -0.5 * Math.PI
+    
+    plane.position.set(15, 0, 0)
+    
+    scene.add(plane)
+    
+    val boxGeometry = new BoxGeometry(4, 4, 4)
+    
+    val material = new MeshBasicMaterial(js.Dynamic.literal(
+      color = new Color().setHex(0xff0000),
+      wireframe = true
+    ).asInstanceOf[MeshBasicMaterialParameters])
+    
+    val cube = new Mesh( boxGeometry, material )
+    
+    cube.position.set(-4, 3, 0)
+           
+    scene.add(cube)
+    
+    
+    val sphereGeometry = new SphereGeometry(4, 20, 20)
+    val sphereMaterial = new MeshBasicMaterial(js.Dynamic.literal(
+      color = new Color().setHex(0x7777ff),
+      wireframe = true
+    ).asInstanceOf[MeshBasicMaterialParameters])
+    
+    val sphere = new Mesh(sphereGeometry, sphereMaterial)
+    
+    sphere.position.set(20, 4, 2)
+    
+    scene.add( sphere )
+    
+    camera.position.set(-30, 40, 30)
+              
+    camera.lookAt(scene.position)
+    
+    document.body.appendChild( renderer.domElement )     
+      
+    renderer.render( scene, camera )
+  }
+}
--- server/src/main/scala/server/Page.scala
+++ server/src/main/scala/server/Page.scala
@@ -4,16 +4,15 @@ import scalatags.Text.all._
 
 object Page {
   
-  def htmlContent(client: String = "Client", elementId: String = "content") = html(
+  def htmlContent(client: String = "Client") = html(
     head(
       meta(charset := "UTF-8"),
       script(src:= "/client-fastopt.js"),
       script(src:= "/client-jsdeps.js")
     ),
     body(
-      style:= "canvas: { height: 100%; width: 100% }",
-      onload:= s"$client().main(document.getElementById('$elementId'))",
-      div(id:=elementId)
+      style:= "margin: 0; overflow: hidden;",
+      onload:= client + "().main()"
     )
   ).render
 }
